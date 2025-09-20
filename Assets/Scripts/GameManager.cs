8038851233416ee2b800412b4f2b57c4d6a04546
CydigitDeveloper <137225069+CydigitDeveloper@users.noreply.github.com>
2025-09-20 12:39:02 +0300

    Version2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,28 @@
 using UnityEngine;
 using System.Collections.Generic;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
+
     public List<PlayerController> allPlayers;
+    public GameObject levelFailedPanel;
+    public TextMeshProUGUI moveCountText;
+    public int moveCount = 10;
+
     private Vector3 moveDirectionThisFrame = Vector3.zero;
 
     private PlayerInputHandler playerInputHandler;
 
     private void Awake()
     {
+        Instance = this;
+
         playerInputHandler = GetComponent<PlayerInputHandler>();
         if (allPlayers == null) allPlayers = new List<PlayerController>();
+
+        moveCountText.text = moveCount.ToString();
     }
 
     private void Update()
@@ -20,10 +31,45 @@ public class GameManager : MonoBehaviour
 
         moveDirectionThisFrame = Vector3.zero;
 
-        if (playerInputHandler.Forward) { moveDirectionThisFrame = Vector3.forward; playerInputHandler.Forward = false; }
-        else if (playerInputHandler.Backward) { moveDirectionThisFrame = Vector3.back; playerInputHandler.Backward = false; }
-        else if (playerInputHandler.Left) { moveDirectionThisFrame = Vector3.left; playerInputHandler.Left = false; }
-        else if (playerInputHandler.Right) { moveDirectionThisFrame = Vector3.right; playerInputHandler.Right = false; }
+        if (moveCount > 0)
+        {
+            if (playerInputHandler.Forward)
+            {
+                moveDirectionThisFrame = Vector3.forward;
+                playerInputHandler.Forward = false;
+
+                moveCount--;
+                moveCountText.text = moveCount.ToString();
+            }
+            else if (playerInputHandler.Backward)
+            {
+                moveDirectionThisFrame = Vector3.back;
+                playerInputHandler.Backward = false;
+
+                moveCount--;
+                moveCountText.text = moveCount.ToString();
+            }
+            else if (playerInputHandler.Left)
+            {
+                moveDirectionThisFrame = Vector3.left;
+                playerInputHandler.Left = false;
+
+                moveCount--;
+                moveCountText.text = moveCount.ToString();
+            }
+            else if (playerInputHandler.Right)
+            {
+                moveDirectionThisFrame = Vector3.right;
+                playerInputHandler.Right = false;
+
+                moveCount--;
+                moveCountText.text = moveCount.ToString();
+            }
+        }
+        else
+        {
+            levelFailedPanel.SetActive(true);
+        }
 
         if (moveDirectionThisFrame != Vector3.zero)
         {
